03db34c274aafb355fa48d6b1bfe0f0cdd209d5d
PeterBiro <biro.peter.hu@gmail.com>
2019-12-07 14:28:41 +0100

    adding output.txt for 1st exercise and list converter tool

--- file_io_tool.py
+++ file_io_tool.py
@@ -21,9 +21,23 @@ def write_list_to_file(file_name, data_list):
                 target.write(str(line)+'\n')
             else:
                 target.write(''.join(map(str, line))+'\n')
+
+
+"""
+map() is behaving different ways in python 2.x and 3.x
+Use which is suitable for you.
+
+#ver 2.x
+def char_list_to_int_list(char_list):
+    return map(int,char_list)
+"""
+#ver 3.x
+def char_list_to_int_list(char_list):
+    return list(map(int,char_list))
       
 
 def main():
+    return
 
 
 if __name__ == "__main__":
